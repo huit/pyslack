82e312b4c9a74413928be30ed31938a6bd1ef539
Michael Kerry <mkerry@fas.harvard.edu>
2021-03-16 14:12:11 -0400

    update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyslack",
-    version="1.0.2",
+    version="1.0.3",
     author="Michael Kerry",
     author_email="michael_kerry@harvard.edu",
     description="A package to facilitate the use of slack webhook for notifications",
